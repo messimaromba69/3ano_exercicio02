5ec7cd7155a775a944bf952b05102fccb43fc856
messimaromba69 <loboarmandoneto@gmail.com>
2025-02-17 12:38:24 -0300

    17/02 as 12:35

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,16 @@
-MediaSourceHandle.exports = function(grunt) {
+module.exports = function(grunt) {
     grunt.initConfig({
-        pkg: grunt.file.readJson('package.json'),
+        pkg: grunt.file.readJSON('package.json'),
+        sass:{
+            dist:{
+                files:{
+                    'dist/styles/main.css': 'src/styles/main.scss'
+                }
+            }
+        }
     })
-} 
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+    grunt.registerTask('default', ['sass']);
+}
